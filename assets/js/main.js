84137b1a1aa2ed89cf49a5fe96ea47874c9d37f3
Md.Kamal <php7version@gmail.com>
2022-06-29 00:03:17 +0600

    all done some of  js work remain

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $('#hero .owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
-    autoplay:false,
+    autoplay:true,
     dots:false,
     responsive:{
         0:{
@@ -13,6 +13,25 @@ $('#hero .owl-carousel').owlCarousel({
         },
         1000:{
             items:1
+        }
+    }
+});
+
+$('#hotManga .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
         }
     }
 })
