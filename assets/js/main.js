754955b0e2017162d0555e2eae4c2d0f7be8ebf5
Md.Kamal <php7version@gmail.com>
2022-06-28 20:03:47 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$('#hero .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
